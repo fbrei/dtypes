c6a2dfd58ff575e9fc3ff3f2e2ab8807eeaf3139
Felix Brei <felix.brei@posteo.de>
2018-05-27 14:04:33 +0200

    Fixed indentation

--- darray.h
+++ darray.h
@@ -1,15 +1,15 @@
 /**
- *	\file darray.h
+ *  \file darray.h
  *  \brief Page based dynamicly growing array.
  *
- *	This file contains tools and datatypes to
- *	create and manipulate an array based data
- *	structure that is able to grow as needed.
+ *  This file contains tools and datatypes to
+ *  create and manipulate an array based data
+ *  structure that is able to grow as needed.
  *
- *	\date 2018/05/27
- *	\author Felix Brei
+ *  \date 2018/05/27
+ *  \author Felix Brei
  *
- *	\bug No known bugs.
+ *  \bug No known bugs.
  */
 
 #ifndef DARRAY_H_HVPNU9B1
@@ -23,8 +23,8 @@
  * This struct manages data and metadata of a dynamic array.
  */
 typedef struct _DArray {
-  void** data; 		    /**< The 2d array that holds the data stored as void pinters. */
-  size_t num_pages; 	/**< The number of currently used pages. */
+  void** data;        /**< The 2d array that holds the data stored as void pinters. */
+  size_t num_pages;   /**< The number of currently used pages. */
 } DArray;
 
 
