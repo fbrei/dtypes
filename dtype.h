c593ff611e7ed74f59cb8fe5e04ae82d7ece4178
Felix Brei <felix.brei@posteo.de>
2018-05-27 22:01:29 +0200

    Updated global header

---
+++ dtype.h
@@ -0,0 +1,20 @@
+/**
+ *	\file dtype.h
+ *  \brief Metaheader
+ *
+ *	Include all custom datatypes.
+ *
+ *	\date 2018/05/27
+ *	\author Felix Brei
+ *
+ *	\bug No known bugs.
+ */
+
+#ifndef DTYPE_H_F3IFONJS
+#define DTYPE_H_F3IFONJS
+
+#include "darray.h"
+#include "prqueue.h"
+#include "hset.h"
+
+#endif /* end of include guard: DTYPE_H_F3IFONJS */
